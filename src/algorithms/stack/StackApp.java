2405c19dd96ead60864d41b6006934ca56efa5bb
grfoger <84062558+grfoger@users.noreply.github.com>
2021-10-08 10:08:28 +0300

    Update StackApp.java

--- src/algorithms/stack/StackApp.java
+++ src/algorithms/stack/StackApp.java
@@ -8,8 +8,8 @@ public class StackApp {
         stack.push(50);
         stack.push(120);
         stack.push(150);
-        stack.pop;
-        System.out.println(stack.peek==120);
+        stack.pop();
+        System.out.println(stack.peek()==120);
         stack.push(40);
         stack.push(50);
         stack.push(60);
@@ -19,12 +19,12 @@ public class StackApp {
         stack.push(100);
         stack.push(110);
         stack.push(120);
-        System.out.println(stack.peek==100);
-        System.out.println(stack.isFull);
+        System.out.println(stack.peek()==100);
+        System.out.println(stack.isFull());
         for(int i=0;i<10;i++){
-        stack.pop;
+        stack.pop();
         }
-        System.out.println(stack.isEmpty);
+        System.out.println(stack.isEmpty());
 
 
         // Здесь протестировать работу написанного StackInt, показав, что он работает
