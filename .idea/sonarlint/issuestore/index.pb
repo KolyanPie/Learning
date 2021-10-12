d24c4cc05f5fd3b9c7f3917371024896113d3117
Arhip <grfoger@gmail.com>
2021-10-12 11:39:34 +0300

    add new folder for StackGeneric

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,7 +1,3 @@
 
 R
 "src/algorithms/stack/StackInt.java,8\9\89e5aded91f5a32967f6f65bf8f4fb527c08c28b
-R
-"src/algorithms/stack/StackApp.java,4\4\445e7882c58644cdc064812be9638ef6302aa1cd
-9
-	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
---
+++ src/algorithms/stackGeneric/StackGeneric.java
@@ -0,0 +1,71 @@
+package algorithms.stack;
+
+
+/**
+ * Этот класс должен реализовать базовую функциональность стека на основе массива.
+ * Необходимо написать код в пропущенных методах
+ */
+public class StackGeneric {
+
+    private int maxSize;    // Размер массива
+    private int[] stackArray;
+    private int top;        // Индекс вершины стека
+
+    public StackGeneric(int size) {
+        this.maxSize = size;
+        stackArray = new int[maxSize];
+        top = -1;
+    }
+
+    /**
+     * Добавление элемента в стек
+     */
+
+
+    public void push(int value) {
+        if (top==maxSize-1) return;
+        top++;
+        stackArray[top] = value;
+
+    }
+
+    /**
+     * Извлечение элемента
+     */
+
+
+    public int pop() {
+
+        top--;
+        return stackArray[top+1];
+    }
+
+    /**
+     * @return верхний элемент, не удаляя его из стека
+     */
+
+
+    public int peek() {
+        
+        return stackArray[top];
+    }
+
+    /**
+     * @return true, если стек пуст
+     */
+
+
+    public boolean isEmpty() {
+        return top<0;
+
+    }
+
+    /**
+     * @return true, если стек заполнен
+     */
+
+
+    public boolean isFull() {
+        return top==maxSize-1;
+    }
+}
